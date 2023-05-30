5cc4e11e22af083b185a5acf5712f6d0998b527f
Andreé Sandoval <andree.bit@gmail.com>
2023-05-29 21:58:49 -0500

    get all products

--- app/Infrastructure/Adapters/CatalogRepository.php
+++ app/Infrastructure/Adapters/CatalogRepository.php
@@ -3,12 +3,14 @@
 namespace App\Infrastructure\Adapters;
 
 use App\Domain\Ports\ICatalogRepository;
+use App\Infrastructure\Models\Product;
 
 class CatalogRepository implements ICatalogRepository
 {
-
     public function getAllProducts(): array
     {
-        return [];
+        $dbProducts = Product::all()->toArray();
+
+        return array_map(fn ($dbProduct) => new Product($dbProduct['id'], $dbProduct['name'], $dbProduct['price']), $dbProducts);
     }
 }
